a3b6ef7c1b2ce778a0fbbc1ef76b7f6f383e1cb2
Piotr Gąsowski <piotr.gasowski87@gmail.com>
2020-05-14 23:19:22 +0200

    Add project files.

---
+++ Rezystory/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Rezystory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butSet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorBox = new System.Windows.Forms.ComboBox();
+            this.pan1 = new System.Windows.Forms.Panel();
+            this.pan2 = new System.Windows.Forms.Panel();
+            this.pan3 = new System.Windows.Forms.Panel();
+            this.pan4 = new System.Windows.Forms.Panel();
+            this.butRes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butSet
+            // 
+            this.butSet.Location = new System.Drawing.Point(205, 231);
+            this.butSet.Name = "butSet";
+            this.butSet.Size = new System.Drawing.Size(91, 21);
+            this.butSet.TabIndex = 1;
+            this.butSet.Text = "SET";
+            this.butSet.UseVisualStyleBackColor = true;
+            this.butSet.Click += new System.EventHandler(this.butSet_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(19, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 66);
+            this.label1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(156, 231);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(43, 20);
+            this.panel1.TabIndex = 3;
+            // 
+            // colorBox
+            // 
+            this.colorBox.BackColor = System.Drawing.SystemColors.Window;
+            this.colorBox.FormattingEnabled = true;
+            this.colorBox.Items.AddRange(new object[] {
+            "Czarny",
+            "Brązowy",
+            "Czerwony",
+            "Pomarańczowy",
+            "Żółty",
+            "Zielony",
+            "Niebieski",
+            "Fioletowy",
+            "Szary",
+            "Biały",
+            "Złoty",
+            "Srebrny"});
+            this.colorBox.Location = new System.Drawing.Point(29, 230);
+            this.colorBox.Name = "colorBox";
+            this.colorBox.Size = new System.Drawing.Size(121, 21);
+            this.colorBox.TabIndex = 4;
+            this.colorBox.SelectedIndexChanged += new System.EventHandler(this.colorBox_SelectedIndexChanged);
+            // 
+            // pan1
+            // 
+            this.pan1.BackColor = System.Drawing.Color.White;
+            this.pan1.Location = new System.Drawing.Point(29, 54);
+            this.pan1.Name = "pan1";
+            this.pan1.Size = new System.Drawing.Size(24, 117);
+            this.pan1.TabIndex = 5;
+            // 
+            // pan2
+            // 
+            this.pan2.BackColor = System.Drawing.Color.White;
+            this.pan2.Location = new System.Drawing.Point(59, 54);
+            this.pan2.Name = "pan2";
+            this.pan2.Size = new System.Drawing.Size(24, 117);
+            this.pan2.TabIndex = 6;
+            // 
+            // pan3
+            // 
+            this.pan3.BackColor = System.Drawing.Color.White;
+            this.pan3.Location = new System.Drawing.Point(90, 54);
+            this.pan3.Name = "pan3";
+            this.pan3.Size = new System.Drawing.Size(24, 117);
+            this.pan3.TabIndex = 7;
+            // 
+            // pan4
+            // 
+            this.pan4.BackColor = System.Drawing.Color.White;
+            this.pan4.Location = new System.Drawing.Point(124, 54);
+            this.pan4.Name = "pan4";
+            this.pan4.Size = new System.Drawing.Size(24, 117);
+            this.pan4.TabIndex = 8;
+            this.pan4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // butRes
+            // 
+            this.butRes.Location = new System.Drawing.Point(171, 54);
+            this.butRes.Name = "butRes";
+            this.butRes.Size = new System.Drawing.Size(131, 117);
+            this.butRes.TabIndex = 9;
+            this.butRes.Text = "RESULT";
+            this.butRes.UseVisualStyleBackColor = true;
+            this.butRes.Click += new System.EventHandler(this.butRes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 330);
+            this.Controls.Add(this.butRes);
+            this.Controls.Add(this.pan4);
+            this.Controls.Add(this.pan3);
+            this.Controls.Add(this.pan2);
+            this.Controls.Add(this.pan1);
+            this.Controls.Add(this.colorBox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butSet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button butSet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox colorBox;
+        private System.Windows.Forms.Panel pan1;
+        private System.Windows.Forms.Panel pan2;
+        private System.Windows.Forms.Panel pan3;
+        private System.Windows.Forms.Panel pan4;
+        private System.Windows.Forms.Button butRes;
+    }
+}
+
---
+++ Rezystory/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rezystory
+{
+    public partial class Form1 : Form
+    {
+        double first;
+        double second;
+        double third;
+        double fourth;
+        double res;
+        double val;
+        double tol;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.first = first; // wartość odczytywana z pierwszego paska
+            this.second = second; // wartość odczytywana z drugiego paska
+            this.third = third; // wartosć paska trzeciego wskazująca na mnożnik
+            this.fourth = fourth; // wartosc paska czwartego będąca tolerancją +- (w tej zmiennej nie wyrażona w procentach) 
+            this.res = res; // mnożnik
+            this.val = val; //wartość liczbowa wyrażana przez dwa pierwsze paski
+            this.tol = tol; // tolerancja
+            
+
+
+            
+
+        }
+
+        
+
+
+
+
+        
+        private void colorBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            switch (colorBox.SelectedIndex)
+            {
+                case 0:
+                    panel1.BackColor = Color.Black;
+                    val = 0;
+                    res = 1;
+                    
+                    break;
+                case 1:
+                    panel1.BackColor = Color.Brown;
+                    val = 10;
+                    res = 10;
+                    tol = 0.01;
+                    break;
+                case 2:
+                    panel1.BackColor = Color.Red;
+                    val = 20;
+                    res = 100;
+                    tol = 0.02;
+                    break;
+                case 3:
+                    panel1.BackColor = Color.Orange;
+                    val = 30;
+                    res = 1000;
+                    tol = 0;
+                    break;
+                case 4:
+                    panel1.BackColor = Color.Yellow;
+                    val = 40;
+                    res = 10000;
+                    tol = 0;
+                    break;
+                case 5:
+                    panel1.BackColor = Color.Green;
+                    val = 50;
+                    res = 100000;
+                    tol = 0.005;
+                    break;
+                case 6:
+                    panel1.BackColor = Color.Blue;
+                    val = 60;
+                    res = 1000000;
+                    tol = 0.0025;
+                    break;
+                case 7:
+                    panel1.BackColor = Color.Violet;
+                    val = 70;
+                    res = 10000000;
+                    tol = 0.001;
+                    break;
+                case 8:
+                    panel1.BackColor = Color.Gray;
+                    val = 80;
+                    res = 0;
+                    tol = 0.0005;
+                    break;
+                case 9:
+                    panel1.BackColor = Color.AntiqueWhite;
+                    val = 90;
+                    res = 0;
+                    tol = 0;
+                    break;
+                case 10:
+                    panel1.BackColor = Color.Gold;
+                    val = 0; // nie ma wartości w tabeli - do ustawienia
+                    res = 0.1;
+                    tol = 0.05;
+                    break;
+                case 11:
+                    panel1.BackColor = Color.Silver;
+                    val = 0; // nie ma wartości w tabeli - do ustawienia
+                    res = 0.01;
+                    tol = 0.1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void butSet_Click(object sender, EventArgs e)
+        {
+            
+
+            if(pan1.BackColor == Color.White)
+            {
+                pan1.BackColor = panel1.BackColor;
+                first = val;
+                
+
+            }
+            else if(pan2.BackColor == Color.White)
+            {
+                pan2.BackColor = panel1.BackColor;
+                second = val / 10;
+
+            }
+            else if(pan3.BackColor == Color.White)
+            {
+                pan3.BackColor = panel1.BackColor;
+                third = res;
+
+            }else if(pan4.BackColor == Color.White)
+            {
+                pan4.BackColor = panel1.BackColor;
+                fourth = tol;
+            }
+        }
+
+        private void SetRes()
+        {
+            double res1 = ((first + second) *third)/1000;
+            //string endRes = Convert.ToString(res1);
+            label1.Text = String.Format("wartość rezystencji to: {0}kΩ, a z uwzględnieniem tolerancji {1}% daje dokładnie wartość od {2}kΩ do {3}kΩ", res1, fourth*100,res1-(res1*fourth), res1+(res1*fourth) );
+        }
+
+        private void butRes_Click(object sender, EventArgs e)
+        {
+            SetRes();
+        }
+    }
+        
+}
